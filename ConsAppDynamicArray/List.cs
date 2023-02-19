a673cf3a742491124502c398cf13ae39b7a5fa51
Dmitriy Sh <theShDis@gmail.com>
2023-02-20 02:40:30 +0400

    Added ChatGPT code

---
+++ ConsAppDynamicArray/List.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppDynamicArray
+{
+    public class List<T>
+    {
+        private T[] array;
+        private int count;
+
+        public List()
+        {
+            array = new T[4];
+            count = 0;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                array[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (count == array.Length)
+            {
+                ResizeArray();
+            }
+            array[count] = item;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            for (int i = index; i < count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            count--;
+        }
+
+        public void Remove(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(array[i], item))
+                {
+                    RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        private void ResizeArray()
+        {
+            T[] newArray = new T[array.Length * 2];
+            Array.Copy(array, newArray, count);
+            array = newArray;
+        }
+    }
+}
--- ConsAppDynamicArray/Program.cs
+++ ConsAppDynamicArray/Program.cs
@@ -10,6 +10,20 @@ namespace ConsAppDynamicArray
     {
         static void Main(string[] args)
         {
+            ConsAppDynamicArray.List<int> list = new ConsAppDynamicArray.List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Remove(3);
+            list.RemoveAt(1);
+            Console.WriteLine(list.Count); // выводит 3
+            Console.WriteLine(list[0]); // выводит 1
+            Console.WriteLine(list[1]); // выводит 4
+            Console.WriteLine(list[2]); // выводит 5
+
+            Console.ReadKey();
         }
     }
 }
